a9bdb78261a12124146fcb035cd2db376de64417
Svalburg <s.valburg@gmail.com>
2015-12-15 13:39:37 +0100

    Added comments

--- include/StateTuple.h
+++ include/StateTuple.h
@@ -10,22 +10,29 @@ using namespace std;
 class StateTuple
 {
 	public:
+		//stores a variable name and corresponding value
+		//will need to be changed for multi type support
 		struct declaration
 		{
 			string name;
-		int value;
+			int value;
 		};
+		
         StateTuple();
 		StateTuple(vector<declaration> cState, vector<declaration> pState);
+		//returns the value for a variable name
 		int getPStateValue(string name);
 		int getCStateValue(string name);
+		//updates the value for a given variable if it exists, creates it if it doesn't
 		void declarePState(string name, int value);
 		void declareCState(string name, int value);
+		//returns the entire state vector
 		vector<declaration> getPState();
 		vector<declaration> getCState();
         virtual ~StateTuple();
     protected:
 	private:
+		//collections of Componentstates and Programstates
 		vector<declaration> cState;
 		vector<declaration> pState;
 };
--- include/TimeDependentEC.h
+++ include/TimeDependentEC.h
@@ -5,7 +5,13 @@
 
 class TimeDependentEC {
 	public:
+		//this class is a data holder and an instance has to exist for every
+		//component state variable, the rule needs to be an expression, the output
+		//of the rule will be the current time dependent energy cost
+		//associated with the current Componentstate.
 		TimeDependentEC(string componentState, Rule* rule);
+		//calls on the current time dependent energy costs for the given
+		//set of states.
 		int getEnergyCost(StateTuple states);
         string getComponentState();
 		virtual ~TimeDependentEC();
