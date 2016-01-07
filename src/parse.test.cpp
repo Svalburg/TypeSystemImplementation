495a7d7e06905a7de838b937514d184c26513ece
DarkBubble <koenmeel@gmail.com>
2016-01-07 13:43:24 +0100

    Added paper test
    Tested the example from the paper

--- src/parse.test.cpp
+++ src/parse.test.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 #include "parse.h"
+#include "libtypesystem.h"
+#include <vector>
 
 using namespace bitpowder::lib;
 
@@ -45,4 +47,50 @@ TEST(ECAParser, FaultyFunction)
 	auto result2 = ParseECAMemory("function test(x) if x then y:= 8 else y := 5 end; y end y", mp2);
 	EXPECT_EQ(false, (bool)result2);
 }
+
+TEST(ECAParser, EndToEnd)
+{
+    /*The example from the paper, with 
+     n * (i + (t +s) * U)
+    n = 4
+    i = 2
+    t = 3
+    s = 10
+    u = 5
+     */
+    MemoryPool mp;
+    auto result = ParseECAMemory("SoundSystem.on(0); repeat 4 begin SoundSystem.playBeepAtHz(5); System.sleep(0) end; SoundSystem.off(0)", mp);
+    Rule* root = result.result()->getTypeRule();
+    
+    
+    vector<ComponentFunction*> compfuncs;
+    ComponentFunction* soundsystemon = new ComponentFunction("SoundSystem", "on", "zero", new RuleAssign("on", new RuleConst("1"), true), 
+                            new RuleConst("0"), 0);
+    compfuncs.push_back(soundsystemon);
+    ComponentFunction* soundsystemoff = new ComponentFunction("SoundSystem", "off", "zero", new RuleAssign("on", new RuleConst("0"), true), 
+                            new RuleConst("0"), 0);
+    compfuncs.push_back(soundsystemoff);
+    ComponentFunction* soundsystembeeps = new ComponentFunction("SoundSystem", "playBeepAtHz", "hz", new RuleConst("0"), 
+                            new RuleConst("2"), 3);
+    compfuncs.push_back(soundsystembeeps);
+    ComponentFunction* systemsleep = new ComponentFunction("System", "sleep", "zero", new RuleConst("0"), 
+                            new RuleConst("0"), 10);
+    compfuncs.push_back(systemsleep);
+	vector<Function*> funcs;
+	vector<TimeDependentEC*> tdec;
+    TimeDependentEC* soundsystem = new TimeDependentEC("SoundSystem", new RuleBinOp("*", new RuleConst("5"), new RuleVar("on", true)));
+    tdec.push_back(soundsystem);
+	Environment* env = new Environment(0, 0, 0, 0, 0, 0, 0, compfuncs, funcs, tdec);
+    
+    StateTuple* startstate = new StateTuple();
+    startstate->declareCState("on", 0);
+    
+    
+	root->updateEnvironment(env);
+	root->updatePath();
+    
+    EXPECT_EQ(268, root->energy(*startstate));
+    
+}
+
 }
